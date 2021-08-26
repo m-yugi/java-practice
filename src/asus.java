555c5b435d0a9a22332e43cb9fea615a60cc2428
m-yugi <msaiyugandhar333@gmail.com>
2021-08-26 18:14:44 +0530

    this is an improved version of palindrome code

--- src/asus.java
+++ src/asus.java
@@ -1,10 +1,20 @@
 import java.util.*;
 public class asus {
     public static void main(String[] args) {
-        int x=1001;
-        String y=String.valueOf(x);
-        StringBuffer b=new StringBuffer(y);
-        b.reverse();
-        System.out.println(y.equals(String.valueOf(b)));
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int temp = x;
+        int r = 0;
+        while (temp != 0) {
+            int r1 = temp % 10;
+            r = r * 10 + r1;
+            temp = temp / 10;
+        }
+        if (r == x && x >= 0) {
+            System.out.println(true);
+        }
+        else {
+        System.out.println(false);
+        }
     }
 }
